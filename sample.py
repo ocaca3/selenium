ffabab3e079618819364e1de5b800a8cb2426e5d
ocaca3 <kakikukeko134679@gmail.com>
2024-12-07 10:36:39 +0900

    add .gitignore, modify sample.py

--- sample.py
+++ sample.py
@@ -1,18 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
+from selenium.webdriver.chrome.options import Options
 import os
 
+#オプション設定
+options = Options()
+#ブラウザを開いたまま処理終了
+options.add_experimental_option("detach", True)
+# "Chromeは自動テストソフトウェアによって胃制御されています"を非表示にする
+options.add_experimental_option(“excludeSwitches”, [‘enable-automation’])
 
-region_list = {'ap-northeast-1': 'https://ap-northeast-1.console.aws.amazon.com/console/home?region=ap-northeast-1#',
-               'ap-northeast-3': 'https://ap-northeast-3.console.aws.amazon.com/console/home?region=ap-northeast-3#',
-               'us-east-1': 'https://us-east-1.console.aws.amazon.com/console/home?region=us-east-1#'
-               }
+# region_list = {'ap-northeast-1': 'https://ap-northeast-1.console.aws.amazon.com/console/home?region=ap-northeast-1#',
+#                'ap-northeast-3': 'https://ap-northeast-3.console.aws.amazon.com/console/home?region=ap-northeast-3#',
+#                'us-east-1': 'https://us-east-1.console.aws.amazon.com/console/home?region=us-east-1#'
+#                }
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(options=options)
 #AWSコンソールサインイン画面を開く
 browser.get("https://aws.amazon.com/jp/console/")
 elem = browser.find_element(By.XPATH, '//*[@id="aws-page-content-main"]/div[2]/div/div/div[2]/div[2]/div/a')
+elem.click()
 #ログイン実施
 load_dotenv()
 #Enter AccountID
